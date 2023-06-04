f4d8b1a49aa3cbd96cbf3e36f2617362ab7d2009
XenovSeries <135427673+XenovSeries@users.noreply.github.com>
2023-06-04 10:02:21 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,8 +1,6 @@
 #(©)Codexbotz
 
-from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,12 +8,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : @Xenov7x\n○ Language : <code>Python3</code>\n○ Source Code : <a href='https://telegram.me/Sn_botsupport'>Available</a>\n○ Channel : @SupernovaNetwork\n○ Support Group : <a href='https://telegram.me/anime_supernova_chat'>This Channel</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🗿 Close", callback_data = "close")
                     ]
                 ]
             )
